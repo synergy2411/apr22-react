4f876dbf17070c1d7bdb2c8b03c59c40c92e00d4
Sumit K <synergy2411@outlook.com>
2022-04-12 20:34:09 +0530

    prop types added

--- src/Components/Expenses/ExpenseFilter/ExpenseFilter.js
+++ src/Components/Expenses/ExpenseFilter/ExpenseFilter.js
@@ -1,6 +1,10 @@
+import PropTypes from 'prop-types';
+
 const ExpenseFilter = props => {
     return (
-        <select className="form-control" value={props.selectedYear} onChange={(event) => props.onFilterYear(event.target.value)}>
+        <select className="form-control" 
+            value={props.selectedYear} 
+            onChange={(event) => props.onFilterYear(event.target.value)}>
             <option value="2019">2019</option>
             <option value="2020">2020</option>
             <option value="2021">2021</option>
@@ -9,4 +13,8 @@ const ExpenseFilter = props => {
     )
 }
 
+ExpenseFilter.propTypes = {
+    selectedYear : PropTypes.string.isRequired,
+    onFilterYear : PropTypes.func.isRequired
+}
 export default ExpenseFilter;
